f9936ac3a344aa1b14a4fa4dd4cefa6e31cb920a
Aryan-2104 <purohitaryan2104@gmail.com>
2025-04-21 21:50:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scroll effect for all links (if you want smooth scrolling)
+document.querySelectorAll('a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
